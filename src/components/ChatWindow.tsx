d73ec5b4c4c1eac4e320de6bdb2c131795837e7a
Ankush Rana <ankush.rana@bsc.es>
2025-06-20 16:53:49 +0200

    change default search

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -280,7 +280,7 @@ const ChatWindow = ({ id }: { id?: string }) => {
   const [files, setFiles] = useState<File[]>([]);
   const [fileIds, setFileIds] = useState<string[]>([]);
 
-  const [focusMode, setFocusMode] = useState('bscDocuments');
+  const [focusMode, setFocusMode] = useState('webSearch');
   const [optimizationMode, setOptimizationMode] = useState('balanced');
 
   const [isMessagesLoaded, setIsMessagesLoaded] = useState(false);
